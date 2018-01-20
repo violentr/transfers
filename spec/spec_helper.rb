ffd6bec48bd86ec52ef29571b522a991a538ed43
Deniss <den.b@itservicesgroup.co.uk>
2018-01-20 12:09:42 +0000

    Add Spec config factory_bot_rails and faker

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
+require 'faker'
+require 'factory_bot_rails'
 RSpec.configure do |config|
+
+  config.include FactoryBot::Syntax::Methods
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
