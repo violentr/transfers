f6bd818903a7e8276da8b43a6a831b8403bc0c68
Deniss <den.b@itservicesgroup.co.uk>
2018-01-20 13:21:10 +0000

    Add Spec User is invalid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true, length: { maximum: 20}
+  validates :last_name, presence: true, length: { maximum: 20}
+  validates :address_line_1, presence: true, length: { maximum: 50}
+  validates :dob, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,14 +7,39 @@ RSpec.describe User, type: :model do
   it { is_expected.to respond_to(:dob) }
 
   describe "User" do
-    [:first_name, :last_name, :address_line_1, :dob].each do |attribute|
-      constraits = OpenStruct.new(first_name: 20, last_name: 20, address_line_1: 50, dob: Date)
+    constraits = OpenStruct.new(first_name: 20, last_name: 20, address_line_1: 50, dob: Date)
+    model_attributes = [:first_name, :last_name, :address_line_1]
 
-      context "when has valid #{attribute}" do
-        it "#{attribute} should be #{constraits.send(attribute)} " do
-          user = create(:user)
-          expect(user).to be_valid
+    context "when valid " do
+      before do
+        @user = create(:user)
+      end
+      model_attributes.each do |attribute|
+
+        it "#{attribute} should be  <= #{constraits.send(attribute)} chars" do
+          expect(@user).to be_valid
         end
+      end
+      it 'dob should be a Date' do
+        expect(@user.dob).to be_a(Date)
+      end
+    end
+    context "when invalid " do
+      before do
+        long_first_name = ('a'..'z').to_a.join
+        long_last_name = ('a'..'z').to_a.join
+        address_line_1 = long_first_name.concat(long_first_name*2)
+        dob = 'some_date'
+        @user = build(:user, first_name: long_first_name,
+                      last_name: long_last_name, address_line_1: address_line_1, dob: dob)
+      end
+      model_attributes.each do |attribute|
+        it "#{attribute} should be > #{constraits.send(attribute)} chars " do
+          expect(@user.valid?).to eq(false)
+        end
+      end
+      it 'dob should not be a Date' do
+        expect(@user.dob).not_to be_a(Date)
       end
     end
   end
