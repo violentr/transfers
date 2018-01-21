9d5374784c57b89f127004d2bac7273e13825e88
Deniss <den.b@itservicesgroup.co.uk>
2018-01-21 01:29:30 +0000

    Add route user login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   end
 
   post '/users/register', controller: 'users', action: 'register'
+  post '/users/login', controller: 'users', action: 'login'
   #
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
