22c71b5d28bd79024bf42ecacaf9712f7173dc98
Deniss <den.b@itservicesgroup.co.uk>
2018-01-21 16:19:11 +0000

    Add Authenticate user functionality to Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+  attr_reader :current_user
+
+  include ExceptionHandler
+
+  private
+
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.new(request.headers).call[:user]
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
 end
